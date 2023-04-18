4afc650b11112b0c7687409217864dc639f3fd48
Ali Mo'nes <AliIsHomie@outlook.com>
2023-04-18 02:39:20 +0200

    fixed broken path

--- javascript/Chess.js
+++ javascript/Chess.js
@@ -5,7 +5,7 @@ class ChessDrawer extends Drawer {
             8, 8,                                               // rows, columns
             color(255, 255, 255, 255), color(60, 60, 60, 255),  // primary and secondary colours
             1,                                                  // stroke weight
-            loadImage('/assets/chess_pieces.png'),              // sprite sheet
+            loadImage('/javascript/assets/chess_pieces.png'),    // sprite sheet
             800 / 6, 267 / 2                                    // piece width and height
         );
 
