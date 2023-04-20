080b76a614ea501b449a911a2173d671227a5886
nasr7322 <120938183+nasr7322@users.noreply.github.com>
2023-04-21 00:13:54 +0200

    changed test function and added gaps for numbers

--- Scala/scalaengine/src/main/scala/test.scala
+++ Scala/scalaengine/src/main/scala/test.scala
@@ -1,8 +1,10 @@
 import java.awt.{Color, Graphics, Graphics2D, BasicStroke}
 import javax.swing.{JFrame, JPanel}
+import scala.swing.event.WindowClosed
+import javax.swing.WindowConstants
 
 object BoardDrawer extends App {
-  def drawBoard(drawMethod: (Graphics, Int, Int, Int, Int, Color, Color) => Unit, bgColor: Color, rows: Int, cols: Int, color1: Color, color2: Color, shape: String): Unit = {
+  def drawBoard(bgColor: Color, rows: Int, cols: Int, color1: Color, color2: Color, shape: String): Unit = {
     val frame = new JFrame("Board Drawer")
     val panel = new JPanel {
       override def paintComponent(g: Graphics): Unit = {
@@ -10,19 +12,19 @@ object BoardDrawer extends App {
         g.fillRect(0, 0, getWidth, getHeight)
         val g2d = g.asInstanceOf[Graphics2D] // cast to Graphics2D
         g2d.setStroke(new BasicStroke(5)) // set line width to 5
-        val tileSize = Math.min(getWidth / cols, getHeight / rows)
+        val tileSize = Math.min((getWidth-100) / cols, (getHeight-100) / rows)
         for {
           row <- 0 until rows
           col <- 0 until cols
         } {
-          val x = col * tileSize
-          val y = row * tileSize
+          val x = ( col * tileSize ) +50
+          val y = ( row * tileSize ) +50
           val tileColor = if ((row + col) % 2 == 0) color1 else color2
           g.setColor(tileColor)
           shape match {
-            case "line" => {
+            case "line" => { if (row != rows-1 && col != cols-1) {
                             g.drawLine(x+tileSize, 0, x+tileSize , rows * tileSize) 
-                            g.drawLine(0, y+tileSize, cols*tileSize , y+tileSize)}
+                            g.drawLine(0, y+tileSize, cols*tileSize , y+tileSize)}}
             case "square" => g.fillRect(x, y, tileSize, tileSize)
             case "circle" => g.fillOval(x, y, tileSize, tileSize)
             case _ => throw new IllegalArgumentException(s"Unsupported shape: $shape")
@@ -30,25 +32,13 @@ object BoardDrawer extends App {
         }
       }
     }
-    //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) dosn't work somehow
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
     frame.getContentPane.add(panel)
     frame.pack()
     frame.setVisible(true)
     frame.setSize(600, 600)
-    drawMethod(panel.getGraphics, 0, 0, panel.getWidth, panel.getHeight, color1, color2)
   }
 
-  def drawTwoColorBoard(g: Graphics, x: Int, y: Int, width: Int, height: Int, color1: Color, color2: Color): Unit = {
-    g.setColor(color1)
-    g.fillRect(x, y, width / 2, height)
-    g.setColor(color2)
-    g.fillRect(x + width / 2, y, width / 2, height)
-  }
+  drawBoard(Color.BLUE, 8, 8, Color.BLACK, Color.WHITE, "square")
 
-  def drawOneColorBoard(g: Graphics, x: Int, y: Int, width: Int, height: Int, color1: Color, color2: Color): Unit = {
-    g.setColor(color1)
-    g.fillRect(x, y, width, height)
-  }
-
-  drawBoard(drawTwoColorBoard, Color.WHITE, 3, 3, Color.RED, Color.RED, "line")
 }
