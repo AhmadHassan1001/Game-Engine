c0330b5b75ecf9b0ffb84cad0b996550ae06cc2e
Ahmad Hassan <43483116+MidoHassan@users.noreply.github.com>
2023-04-22 21:59:11 +0200

    validate pawn move

--- javascript/ChessController.js
+++ javascript/ChessController.js
@@ -1,15 +1,33 @@
-const orderAlpha=(c)=>{
 
-    return c.charCodeAt(0) - "a".charCodeAt(0);
-}
-const orderDigit=(c)=>{
-
-    return c.charCodeAt(0) - "0".charCodeAt(0);
-}
 const move=(controller,pos1,pos2)=>{
-    // TODO: Validate parameters
-    // TODO: Validate action
+    valid=true;
+    // Validate parameters
 
+    // validate first parameter
+    valid&&=(pos1.length==2&&isAlpha(pos1[0])&&isDigit(pos1[1])&&orderAlpha(pos1[0])<=orderAlpha('h')&&orderDigit(pos1[1])<=orderDigit('8')&&orderDigit(pos1[1])>=orderDigit('1'));
+    // validate second parameter
+    valid&&=(pos2.length==2&&isAlpha(pos2[0])&&isDigit(pos2[1])&&orderAlpha(pos2[0])<=orderAlpha('h')&&orderDigit(pos2[1])<=orderDigit('8')&&orderDigit(pos2[1])>=orderDigit('1'));
+
+
+    // Validate action
+    // validate there is object to move
+    let origin=controller.drawerMap[8-1-(orderDigit(pos1[1])-1)][orderAlpha(pos1[0])];
+    let target=controller.drawerMap[8-1-(orderDigit(pos2[1])-1)][orderAlpha(pos2[0])];
+    valid&&=origin!=null;
+
+    // for each piece validate action
+
+    if(origin.type=='pawn'){
+        validpawn=false;
+        //front cell is empty
+        validpawn||=orderDigit(pos2[1])==(orderDigit(pos1[1])+((origin.labels.includes('white'))?1:-1))&&Math.abs(orderAlpha(pos2[0])-orderAlpha(pos1[0]))==1&&target==null;
+        //corner cells has enemy
+        validpawn||=orderDigit(pos2[1])==(orderDigit(pos1[1])+((origin.labels.includes('white'))?1:-1))&&Math.abs(orderAlpha(pos2[0])-orderAlpha(pos1[0]))==1&&target!=null&&(target.labels.includes('black')&&origin.labels.includes('white')||target.labels.includes('white')&&origin.labels.includes('black'));
+        valid&&=validpawn;
+        console.log(Math.abs(orderAlpha(pos2[0])-orderAlpha(pos1[0]))==1);
+    }
+
+    if(!valid)return;
     // Move
     console.log(`move  ${8-1-(orderDigit(pos1[1])-1)} ${orderAlpha(pos1[0])}`)
     controller.drawerMap[8-1-(orderDigit(pos2[1])-1)][orderAlpha(pos2[0])]=controller.drawerMap[8-1-(orderDigit(pos1[1])-1)][orderAlpha(pos1[0])];
--- javascript/Piece.js
+++ javascript/Piece.js
@@ -1,16 +1,18 @@
 class Piece {
     
-    constructor(i, j, pieceWidth, pieceHeight) {
+    constructor(i, j, pieceWidth, pieceHeight,type) {
         this.pieceWidth = pieceWidth;
         this.pieceHeight = pieceHeight;
         this.x = pieceWidth * j;
         this.y = pieceHeight * i;
+        this.labels=[]
+        this.type=type
     }
 }
 
 class ChessPiece extends Piece {
     
-    constructor(i, j) {
-        super(i, j, 800 / 6, 267 / 2);
+    constructor(i, j,type) {
+        super(i, j, 800 / 6, 267 / 2,type);
     }
 }
