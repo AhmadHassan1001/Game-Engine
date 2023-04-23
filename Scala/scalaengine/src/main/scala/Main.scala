6a16f76c8915fe972fd555fd029359f2f3da149f
marwan <maroelsays2013@gmail.com>
2023-04-23 23:44:56 +0200

    King Remaning

--- Scala/scalaengine/src/main/scala/Main.scala
+++ Scala/scalaengine/src/main/scala/Main.scala
@@ -92,7 +92,7 @@ object GameEngine {
     def InBoard(input: String,rows:Int,cols:Int):Boolean = {
 
       if(input.size != 4)false
-
+      
       var start:Tuple2[Int,Int] = (input(0)-'a'+1,input(1).asDigit)
       if(start._1 <= 0 || start._1 > cols )false
       if(start._2 <= 0 || start._2 > rows )false
@@ -109,50 +109,73 @@ object GameEngine {
 
     val Chess_Controller = (input: String,rows:Int,cols:Int,turn:Int /*0->Black,1->white*/) => {
 
-      if(!InBoard(input,rows,cols))false
+      //Check if Start and end Position are in Board*/
+      var ret = true
+      if(!InBoard(input,rows,cols))ret =  false
 
+      /*tuples containing position of peice in the Chess array (row,column)*/
       var start:Tuple2[Int,Int] = (Math.abs(input(1).asDigit-rows),input(0)-'a')
       var end  :Tuple2[Int,Int] = (Math.abs(input(3).asDigit-rows),input(2)-'a')
       
-      if(chessBoard(start._1)(start._2) == null) false
+      /*if start peice is null then return false*/
+      if(chessBoard(start._1)(start._2) == null)ret = false
+      
+      /*Check Validty of Start Position*/
+      if(chessBoard(start._1)(start._2) != null)
+      {
+        /*if peice at start position is black and it's white turn then return false*/
+        if(chessBoard(start._1)(start._2)._1 > 6 && turn == 1)ret = false
 
-      if(chessBoard(start._1)(start._2) != null){
-        if(chessBoard(start._1)(start._2)._1 > 6 && turn == 1)false
-        if(chessBoard(start._1)(start._2)._1 < 6 && turn == 0)false
+        /*if peice at start position is white and it's Black white turn then return false*/
+        if(chessBoard(start._1)(start._2)._1 <= 6 && turn == 0)ret = false
       }
 
-      if(chessBoard(end._1)(end._2) != null){
-        if(chessBoard(end._1)(end._2)._1 > 6 && turn == 0)false
-        if(chessBoard(end._1)(end._2)._1 < 6 && turn == 1)false
+      /*Check Validty of end Position*/
+      if(chessBoard(end._1)(end._2) != null)
+      {
+        /*if peice at end position is Black and it's Black turn then return false*/
+        println(chessBoard(end._1)(end._2)._1)
+        if(chessBoard(end._1)(end._2)._1 > 6 && turn == 0)ret = false
+
+        /*if peice at end position is White and it's white turn then return false*/
+        if(chessBoard(end._1)(end._2)._1 <= 6 && turn == 1)ret = false
       }      
 
-      var peiceType = 1
+      var peiceType = 14
       if(chessBoard(start._1)(start._2) != null){
+        //Get the type of starting Peice
         peiceType = chessBoard(start._1)(start._2)._1
       }
-      
-
-      var deltaX = end._2 - start._2
+  
+      var deltaX = end._2 - start._2 
       var deltaY = end._1 - start._1
       var dirX = 0
       var dirY = 0
+
+      //Get the direction Vector's X
       if(deltaX > 0)dirX = 1
       else if(deltaX < 0)dirX = -1
 
+      //Get the direction Vector's Y
       if(deltaY > 0)dirY = 1
       else if(deltaY < 0)dirY = -1
 
       def rookMove():Boolean={
 
-        if(Math.abs(deltaY) > 0 && Math.abs(deltaX) > 0)false
+        var accept = true
+        //if both Change in X and Change in Y are not zero then this move is Invalid
+        if(Math.abs(deltaY) > 0 && Math.abs(deltaX) > 0)accept = false
 
         var startY = start._1
         var startX = start._2
-        var accept = true
-        while(startY != end._1 || startX != end._2){
+        
+        
+        //go in the direction of the Move
+        while((startY != end._1 || startX != end._2) && accept){
           startY += dirY
           startX +=dirX
-        
+          
+          //if there is any peice in the pass then this move is InValid So return false
           if((chessBoard(startY)(startX) != null) && (startY != end._1 || startX != end._2)) 
             accept = false         
         }
@@ -161,15 +184,62 @@ object GameEngine {
       }
 
       def knightMove():Boolean={
-        false
+
+        /*Valid Moves for knight are only those whose cahnge in one of its dimensions is 2 
+          and the other is 1, otherwise this Move is In valid*/
+        if((Math.abs(deltaY) == 2 && Math.abs(deltaX) == 1) || 
+          (Math.abs(deltaY) == 1 && Math.abs(deltaX) == 2))
+          true
+        else
+          false 
       }
 
       def bishopMove():Boolean={
-        false
+        
+        var accept = true
+        /*Valid Moves for Bishop are only Moves Where absolute Change in x equals absolute Change in Y*/
+        if(Math.abs(deltaY) == Math.abs(deltaX))
+        {
+          var startY = start._1
+          var startX = start._2
+        
+          //go in the direction of the Move
+          while(startY != end._1 || startX != end._2){
+            startY += dirY
+            startX +=dirX
+            
+            //if there is any peice in the pass then this move is InValid So return false
+            if((chessBoard(startY)(startX) != null) && (startY != end._1 || startX != end._2)) 
+              accept = false         
+          }
+        }
+        else
+          accept = false
+
+        accept
       }
 
       def queenMove():Boolean={
-        false
+
+        var accept = true
+        /*only the none acceptable Move is the Move when change in x and Y are not equal and none of them are zeros*/
+        if(Math.abs(deltaY) > 0 && Math.abs(deltaX) > 0 && Math.abs(deltaY) != Math.abs(deltaX))accept = false
+        
+        var startY = start._1
+        var startX = start._2
+        
+        //go in the direction of the Move
+        while((startY != end._1 || startX != end._2) && accept)
+        {
+          startY +=dirY
+          startX +=dirX
+            
+          //if there is any peice in the pass then this move is InValid So return false
+          if((chessBoard(startY)(startX) != null) && (startY != end._1 || startX != end._2)) 
+              accept = false         
+        }
+
+        accept
       }
 
       def kingMove():Boolean={
@@ -177,7 +247,25 @@ object GameEngine {
       }
 
       def pawnMove():Boolean={
-        true
+
+        var accept = false
+        var startY = start._1
+        var startX = start._2
+
+        if(turn == 0){
+          if(deltaY == -1 && deltaX == 0 && chessBoard(startY-1)(startX) == null)accept = true
+          if(deltaY == -1 && deltaX == -1 && chessBoard(startY-1)(startX-1) != null)accept = true
+          if(deltaY == -1 && deltaX == 1 && chessBoard(startY-1)(startX+1) != null)accept = true
+          if(deltaY == -2 && startY == 6 && chessBoard(startY-2)(startX) == null)accept = true
+        }
+        else{
+          if(deltaY == 1 && deltaX == 0 && chessBoard(startY+1)(startX) == null)accept = true
+          if(deltaY == 1 && deltaX == -1 && chessBoard(startY+1)(startX-1) != null)accept = true
+          if(deltaY == 1 && deltaX == 1 && chessBoard(startY+1)(startX+1) != null)accept = true
+          if(deltaY == 2 && startY == 1 && chessBoard(startY+2)(startX) == null)accept = true
+        }
+      
+        accept
       }
 
       var valid = peiceType match{
@@ -187,10 +275,16 @@ object GameEngine {
         case 4|10 => queenMove
         case 5|11 => kingMove
         case 6|12 => pawnMove
+        case _ => false
       }
       
-      if(valid)true
-      else false
+      if(ret){  
+        if(valid )true
+        else false
+      }
+      else{
+        false
+      }
 
     }: Boolean
 
@@ -254,12 +348,12 @@ object GameEngine {
               input = Games(j);
               
               input match {
-                  case "Chess"    => abstractEngine(Color.GRAY, 8, 8, new Color(0xC2B280), Color.WHITE, "square",2,drawBoard,Chess_Controller)
-                  case "8Queens"  => abstractEngine(Color.DARK_GRAY, 8, 8, new Color(0xC2B280), Color.WHITE, "square",2,drawBoard,Queens_Controller)
-                  case "Connect4" => abstractEngine(Color.BLUE, 6, 8, Color.WHITE, Color.WHITE, "circle",1,drawBoard,Connect4_Controller)
-                  case "XO"       => abstractEngine(Color.BLACK, 3, 3, Color.YELLOW, Color.YELLOW, "line",1,drawBoard,XO_Controller)
-                  case "Suduko"   => abstractEngine(Color.LIGHT_GRAY, 9, 9, Color.BLACK, Color.WHITE, "line",1,drawBoard,Suduko_Controller)
-                  case "Checkers" => abstractEngine(Color.DARK_GRAY, 8, 8, new Color(0xC2B280), Color.WHITE,"square",2,drawBoard,Checkers_Controller)
+                  case "Chess"    => abstractEngine(Color.GRAY, 8, 8, new Color(0xC2B280), Color.WHITE, "square",2,input,drawBoard,Chess_Controller)
+                  case "8Queens"  => abstractEngine(Color.DARK_GRAY, 8, 8, new Color(0xC2B280), Color.WHITE, "square",2,input,drawBoard,Queens_Controller)
+                  case "Connect4" => abstractEngine(Color.BLUE, 6, 8, Color.WHITE, Color.WHITE, "circle",1,input,drawBoard,Connect4_Controller)
+                  case "XO"       => abstractEngine(Color.BLACK, 3, 3, Color.YELLOW, Color.YELLOW, "line",1,input,drawBoard,XO_Controller)
+                  case "Suduko"   => abstractEngine(Color.LIGHT_GRAY, 9, 9, Color.BLACK, Color.WHITE, "line",1,input,drawBoard,Suduko_Controller)
+                  case "Checkers" => abstractEngine(Color.DARK_GRAY, 8, 8, new Color(0xC2B280), Color.WHITE,"square",2,input,drawBoard,Checkers_Controller)
               }
 
               dispose()
@@ -276,7 +370,8 @@ object GameEngine {
     }
 
     def abstractEngine(
-        bgColor: Color,rows: Int,cols: Int,color1: Color,color2: Color,shape: String,typ:Int,
+        bgColor: Color,rows: Int,cols: Int,color1: Color,color2: Color,
+        shape: String,typ:Int,gameName:String,
         Drawer: (Color,Int,Int,Color,Color,String,Graphics2D) => Unit,
         Controller: (String,Int,Int,Int) => Boolean
     ): Unit = {
@@ -333,22 +428,35 @@ object GameEngine {
               preferredSize = new Dimension(670,20)
               background = new Color(0xb1e9fe)
 
+              var txt = gameName match{
+                case "Chess"|"8Queens"|"Checkers" => "Black's  Turn"
+                case "XO" | "Connect4" => "Player's 1 Turn"
+              }
+
               var inputField1 = new TextField("")
               var inputField2 = new TextField("")
+              var label = new Label(txt)
               //Adjust Size
               inputField1.maximumSize = new Dimension(100,30)
               inputField2.maximumSize = new Dimension(100,30)
               //Adjust Font
               inputField1.font = new Font("Arial", 0, 15)
               inputField2.font = new Font("Arial", 0, 15)
+              //label adj
+              label.maximumSize = new Dimension(100,20)
+              label.font = new Font("Arial", 1, 15)
+              label.foreground = new Color(0x013220)
              
               contents += Swing.HStrut(15)
               contents += inputField1
               contents += Swing.HStrut(35)
+              
               if(typ == 2)contents += inputField2
               else contents += Swing.HStrut(100)
 
-              contents += Swing.HStrut(320)
+              contents += Swing.HStrut(80)
+              contents += label
+              contents += Swing.HStrut(140)
 
               var turn = 0
               contents += Button("Do Action!!") { 
@@ -366,14 +474,28 @@ object GameEngine {
                     chessBoard(Math.abs(s2(1).asDigit-rows))(s2(0)-'a') = chessBoard(Math.abs(s1(1).asDigit-rows))(s1(0)-'a')
                     chessBoard(Math.abs(s1(1).asDigit-rows))(s1(0)-'a') = null
 
+                    turn+=1
+                    turn = turn%2  
+
+                    if(turn%2 == 0){
+                      gameName match{
+                        case "Chess"|"8Queens"|"Checkers" => label.text = "Black's  Turn"
+                        case "XO" | "Connect4" => label.text = "Player's 1 Turn"
+                      }
+                    } 
+                    else {
+                      gameName match{
+                        case "Chess"|"8Queens"|"Checkers" => label.text = "White's  Turn"
+                        case "XO" | "Connect4" => label.text = "Player's 2 Turn"
+                      }
+                    }
                     canvas.repaint()
                   }
                   else{
-                    println("Not valid")
-                  }
-
-                turn+=1
-                turn = turn%2         
+                    label.foreground = Color.RED
+                    label.text = "Not valid"
+                    label.foreground = new Color(0x013220)
+                  }       
               }
             }
 
@@ -391,104 +513,3 @@ object GameEngine {
     }
   }
 }
-
-
-
-/*
-contents = new BoxPanel(Orientation.Vertical) {
-  listenTo(mouse.clicks)
-  listenTo(keys)
-  reactions += {
-    case MouseClicked(_, p, _, _, _) => println(p)
-          requestFocus()
-    case KeyTyped(_, c, _, _) => println(s"Key typed: $c")
-          requestFocus()
-  }
-
-  override def paint(g: Graphics2D): Unit = {
-
-  }
-}
-
-val chess_Drawer = (g: Graphics2D) => {
-      g.setColor(new Color(0xffffff))
-      g.fillRect(120, 80, 440, 440)
-      g.setColor(new Color(100, 100, 100))
-      for {
-        i <- 0 to 7
-        j <- 0 to 7
-      } {
-
-        if ((i + j) % 2 == 0) g.setColor(new Color(0xeeeed2))
-        else g.setColor(new Color(0, 0, 0))
-
-        g.fillRect(140 + i * 50, 100 + j * 50, 50, 50)
-      }
-    }
-
-    val connect4_Drawer = (g: Graphics2D) => {
-      g.setColor(new Color(0x06038d))
-      g.fillRoundRect(150, 90, 400, 440, 10, 10)
-      g.setColor(new Color(255, 255, 255))
-      for {
-        i <- 0 to 5
-        j <- 0 to 6
-      } {
-        g.fillOval(185 + i * 55, 120 + j * 55, 50, 50)
-      }
-    }
-
-    val XO_Drawer = (g: Graphics2D) => {
-      g.setColor(new Color(139, 211, 230))
-      g.fillRoundRect(45, 45, 600, 520, 10, 10)
-      g.setColor(new Color(206, 162, 0))
-      g.setStroke(new BasicStroke(10))
-
-      g.drawLine(246, 50, 246, 560)
-      g.drawLine(442, 50, 442, 560)
-      g.drawLine(50, 220, 640, 220)
-      g.drawLine(50, 390, 640, 390)
-    }
-
-    /*to be implemented*/
-    val Suduko_Drawer = (g: Graphics2D) => {
-      g.setColor(new Color(139, 211, 230))
-      g.fillRoundRect(45, 45, 600, 520, 10, 10)
-      g.setColor(new Color(206, 162, 0))
-      g.setStroke(new BasicStroke(10))
-
-      g.drawLine(246, 50, 246, 560)
-      g.drawLine(442, 50, 442, 560)
-      g.drawLine(50, 220, 640, 220)
-      g.drawLine(50, 390, 640, 390)
-    }
-
-  var textField = new TextField()
-  textField.background = Color.white
-  textField.font = new Font("Arial",1,16)
-  textField.maximumSize = new Dimension(600,60)
-  textField.minimumSize = new Dimension(600,60)
-  textField.preferredSize = new Dimension(600,60)
-  textField.xLayoutAlignment_=(0f)
-
-  var button = new Button("input")
-  button.xLayoutAlignment_=(600f)
-
-  contents += Swing.VStrut(600) 
-  contents += textField
-  contents += button
-
-
-      
-    for{
-      i <- 0 until 8
-      j <- 0 until 8
-    }{
-      print(chessBoard(i)(j))
-      if(j == 7)println()
-    }
-
-  //val file = new File("src/main/resources/Chess/BishopBlack.png")
-  // val image = ImageIO.read(file)
-
-*/
