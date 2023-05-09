d2a4cf50228db176fd82e438b151a265b953b9fe
Ahmad Hassan <43483116+MidoHassan@users.noreply.github.com>
2023-05-09 12:23:51 +0300

    fix queen+add almost all checker fules

--- javascript/Checker.js
+++ javascript/Checker.js
@@ -1,11 +1,11 @@
 /**
  * 
  * Rules:
- * each piece can move diagonally forward initially
- * if piece reached the opposite last row it becomes king and can move diagonally up or down(TODO)
- * piece move is only one square apart
- * piece can jump the opponent which removes the oponent checker
- * you can make many jump in one move (TODO)
+ * each piece can move diagonally forward initially -> done
+ * if piece reached the opposite last row it becomes king and can move diagonally up or down -> done
+ * piece move is only one square apart -> done
+ * piece can jump the opponent which removes the oponent checker -> done
+ * you can make many jump in one move (TODO) [list]
  * 
  */
 class Checker extends AbstractGameEngine{
@@ -13,20 +13,7 @@ class Checker extends AbstractGameEngine{
 
         super(map, rows, columns);
         this.spriteSheet = loadImage('/javascript/assets/checker.png');
-
-    }
-    Drawer(map) {
-        super.Drawer();
-
-        const canvasHeight = 700;
-        const canvasWidth = 700;
-        const offset = 60;
-        const rows = 8;
-        const columns = 8;
-        const primaryColour = color(92, 64, 51, 255);
-        const secondaryColour = color(196, 164, 132, 255);
-        const tileWidth = (canvasWidth - offset) / columns;
-        const tileHeight = (canvasHeight - offset) / rows;
+        
         // white pieces
         map[0][0]=new CheckerPiece('white');
         map[0][2]=new CheckerPiece('white');
@@ -46,6 +33,24 @@ class Checker extends AbstractGameEngine{
         map[7][3]=new CheckerPiece('black');
         map[7][5]=new CheckerPiece('black');
         map[7][7]=new CheckerPiece('black');
+
+        //testing
+        map[6][0]=new CheckerPiece('white');
+        map[7][1]=null;
+
+    }
+    Drawer(map) {
+        super.Drawer();
+
+        const canvasHeight = 700;
+        const canvasWidth = 700;
+        const offset = 60;
+        const rows = 8;
+        const columns = 8;
+        const primaryColour = color(92, 64, 51, 255);
+        const secondaryColour = color(196, 164, 132, 255);
+        const tileWidth = (canvasWidth - offset) / columns;
+        const tileHeight = (canvasHeight - offset) / rows;
 
         this.SquareBoardDrawer(rows, columns, primaryColour, secondaryColour, tileWidth, tileHeight, offset);
         this.DrawText(rows, columns, tileWidth, tileHeight, canvasWidth, canvasHeight);
@@ -91,8 +96,62 @@ class Checker extends AbstractGameEngine{
 
 
         // Validate action
-
         
+        let origin = map[8 - 1 - (orderDigit(pos1[1]) - 1)][orderAlpha(pos1[0])];
+        let target = map[8 - 1 - (orderDigit(pos2[1]) - 1)][orderAlpha(pos2[0])];
+
+        valid &&= origin != null;
+        
+        valid &&= (origin.colour=="black"&&player==1)||(origin.colour=="white"&&player==2)
+
+        if (origin)
+            valid &&= origin.validateMove(pos1,pos2,target,map);
+        
+        if (!valid)
+            return [false, map];
+
+        console.log("move")
+        // for jump
+        if(!origin.isKing){
+            let preposX = 8 - 1 - (orderDigit(pos1[1]) - 1) + ((origin.colour == 'black') ? -1 : 1), preposY = orderAlpha(pos1[0]) + ((orderAlpha(pos2[0]) - orderAlpha(pos1[0])) > 0 ? 1 : -1);
+
+            if (preposX >= 0 && preposX < 8 && preposY >= 0 && preposY < 8) {
+                let pretarget = (map[preposX][preposY]);
+                // it is a jump
+                if(target!=pretarget){
+                    console.log("delete pretarget")
+                    map[preposX][preposY]=null;
+                }
+            }
+            // console.log("target",orderDigit(pos1[1]),target,pretarget,8-1-(orderDigit(pos1[1]) + ((origin.colour == 'black') ? 1 : -1))-1,orderAlpha(pos1[0])+((orderAlpha(pos2[0]) - orderAlpha(pos1[0]))>0?1:-1))
+        
+
+        }else{
+            let preposX = 8 - 1 - (orderDigit(pos1[1]) - 1) + ((orderDigit(pos2[1]) - orderDigit(pos1[1])) > 0 ? -1 : 1), preposY = orderAlpha(pos1[0]) + ((orderAlpha(pos2[0]) - orderAlpha(pos1[0])) > 0 ? 1 : -1);
+            if (preposX >= 0 && preposX < 8 && preposY >= 0 && preposY < 8) {
+                let pretarget = (map[preposX][preposY]);
+                // console.log('index',8 - 1 - (orderDigit(pos1[1]) - 1) + ((orderDigit(pos2[1]) - orderDigit(pos1[1])) > 0 ? -1 : 1),orderAlpha(pos1[0]) + ((orderAlpha(pos2[0]) - orderAlpha(pos1[0])) > 0 ? 1 : -1))
+                // it is a jump
+                if(target!=pretarget){
+                    console.log("delete pretarget")
+                    map[preposX][preposY]=null;
+                }
+            }
+        }
+        
+        //move
+        map[8 - 1 - (orderDigit(pos2[1]) - 1)][orderAlpha(pos2[0])] = map[8 - 1 - (orderDigit(pos1[1]) - 1)][orderAlpha(pos1[0])];
+        map[8 - 1 - (orderDigit(pos1[1]) - 1)][orderAlpha(pos1[0])] = null;
+        // console.log("map[8 - 1 - (orderDigit(pos1[1]) - 1)][orderAlpha(pos1[0])]",map[8 - 1 - (orderDigit(pos1[1]) - 1)][orderAlpha(pos1[0])])
+        console.log('kign validation',map[8 - 1 - (orderDigit(pos2[1]) - 1)][orderAlpha(pos2[0])].colour,8 - 1 - (orderDigit(pos2[1]) - 1)==0,8 - 1 - (orderDigit(pos2[1]) - 1))
+        if(map[8 - 1 - (orderDigit(pos2[1]) - 1)][orderAlpha(pos2[0])].colour=="white"&&8 - 1 - (orderDigit(pos2[1]) - 1)==8-1){
+            console.log("King White")
+            map[8 - 1 - (orderDigit(pos2[1]) - 1)][orderAlpha(pos2[0])].isKing=true;
+        }
+        if(map[8 - 1 - (orderDigit(pos2[1]) - 1)][orderAlpha(pos2[0])].colour=="black"&&8 - 1 - (orderDigit(pos2[1]) - 1)==0){
+            console.log("King Black")
+            map[8 - 1 - (orderDigit(pos2[1]) - 1)][orderAlpha(pos2[0])].isKing=true;
+        }
         return [true, map];
     }
 
